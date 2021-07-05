e1de169dcc17dfdc702191119dd15d9310f0a49a
Nico <Nicod3mo1@outlook.com>
2021-07-05 05:12:17 -0300

    primer commit

---
+++ movies/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+
+class Movie(models.Model):
+    title = models.CharField(max_length=100)
+    genre = models.CharField(max_length=100)
+    year = models.IntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    creator = models.ForeignKey('auth.User', related_name='movies', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return str(self.id) + " " + self.title
+
+    class Meta:
+        ordering = ['-id']
